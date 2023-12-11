ad962dfe5bf0a42a18495a65dd450a5717414c4f
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-12-11 12:40:40 -0300

    ADD: localStorage to save user theme preferences

--- src/themeToggle.js
+++ src/themeToggle.js
@@ -8,6 +8,25 @@ const themeToggle = (theme) => {
         document.documentElement.classList.toggle('dark');
         document.documentElement.classList.contains('dark') ? theme.src = "./assets/dark.png" : theme.src = "./assets/light.png";
 
+        if (document.documentElement.classList.contains('dark')) {
+            localStorage.setItem("darkTheme", true);
+        } else {
+            localStorage.setItem("darkTheme", false);
+        }
+
+    });
+
+    window.addEventListener("load", (e) => {
+
+        e.preventDefault();
+
+        const dark = localStorage.getItem("darkTheme");
+
+        if (dark == "true") {
+            document.documentElement.classList.add('dark');
+            theme.src = "./assets/dark.png";
+        }
+
     });
 
 };
